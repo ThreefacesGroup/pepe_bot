e2a0f3cac02b0e6002fd2debc89175ab729b17da
Osmel Martinez <osmeldev@gmail.com>
2025-07-04 02:39:53 -0400

    Add NGINX configuration and update webhook controller logging
    - Introduce an `nginx.conf` file to set up reverse proxy for the API.
    - Add logging to the webhook controller for received request bodies.

--- src/modules/webhook/presenters/webhook.controller.ts
+++ src/modules/webhook/presenters/webhook.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post} from '@nestjs/common';
+import {Body, Controller, Logger, Post} from '@nestjs/common';
 import { WebhookService } from '../application/webhook.service';
 
 @Controller('webhooks')
 export class WebhookController {
+  private readonly _logger = new Logger(WebhookController.name)
   constructor(private readonly webhookService: WebhookService) {}
 
-  @Post()
-   handleWebhook() {
+  @Post(':id')
+   handleWebhook(@Body() body: any) {
+    this._logger.log(body)
       return { success: true}
   }
 }
